bee0c5de597e52f8bc1a9ef2e757a4f645037980
ibbel <68642712+ibbel@users.noreply.github.com>
2020-07-24 10:50:59 +0200

    Delete inscription.js

--- Page_d'inscription/inscription.js
+++
@@ -1,8 +0,0 @@
-const send = (event) =>{
-    event.preventDefault()
-    for(let elem of event.target){
-        console.log(elem.value)
-    }
-}
-
-document.querySelector("#FormInscription").addEventListener('submit', send)
