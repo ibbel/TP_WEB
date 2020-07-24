e29d0575f5d0922497c3d42edfd739c15e9a161a
ibbel <68642712+ibbel@users.noreply.github.com>
2020-07-24 10:55:22 +0200

    Delete connexion.js

--- TP_WEB/Page_de_Connexion/connexion.js
+++
@@ -1,9 +0,0 @@
-const send = (event) =>{            
-    event.preventDefault()
-    console.log(event)
-    for(let elem of event.target){
-        console.log(elem.value)
-    }
-}
-
-document.querySelector("#FormConnexion").addEventListener('submit', send)
